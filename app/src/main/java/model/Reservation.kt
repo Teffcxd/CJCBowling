30d2c8a43c879d5581b7134d14dd9c4640b8d79b
Jeimy Sanchez <jeimyestefanias@gmail.com>
2026-03-13 15:12:10 -0500

    Modelo de reserva

---
+++ app/src/main/java/model/Reservation.kt
@@ -0,0 +1,11 @@
+package model
+
+data class Reservation(
+    val id: Int = 0,
+    val clientName: String,
+    val phone: String,
+    val lane: Int,
+    val date: String,
+    val time: String,
+    val status: String
+)
