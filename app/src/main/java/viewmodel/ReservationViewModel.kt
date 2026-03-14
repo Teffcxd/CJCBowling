9bad0a7bd89c7875e84847f59b4af3815d2f295b
CesarRodriguez10 <alejorz2004@gmail.com>
2026-03-14 11:07:55 -0500

    Implementar packet repository para le funcionamiento

--- app/src/main/java/viewmodel/ReservationViewModel.kt
+++ app/src/main/java/viewmodel/ReservationViewModel.kt
@@ -1,4 +1,61 @@
-package viewmodel
+package com.example.bowlingapp.viewmodel
 
-class ReservationViewModel {
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.ViewModel
+import model.Reservation
+import repository.ReservationRepository
+
+class ReservationViewModel(
+    private val repository: ReservationRepository
+) : ViewModel() {
+
+    var reservations = mutableStateListOf<Reservation>()
+
+    init {
+        loadReservations()
+    }
+
+    fun loadReservations() {
+
+        reservations.clear()
+
+        reservations.addAll(repository.getAll())
+    }
+
+    fun addReservation(reservation: Reservation) {
+
+        val exists = repository.check(
+            reservation.lane,
+            reservation.date,
+            reservation.time
+        )
+
+        if (!exists) {
+
+            repository.insert(reservation)
+
+            loadReservations()
+        }
+    }
+
+    fun deleteReservation(reservation: Reservation) {
+
+        repository.delete(reservation)
+
+        loadReservations()
+    }
+
+    fun updateReservation(reservation: Reservation) {
+
+        repository.update(reservation)
+
+        loadReservations()
+    }
+
+    fun searchReservation(name: String) {
+
+        reservations.clear()
+
+        reservations.addAll(repository.search(name))
+    }
 }
