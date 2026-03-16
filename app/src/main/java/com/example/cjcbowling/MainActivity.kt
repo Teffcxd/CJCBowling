909dbe73f3e5ac2aed56ebf6268e78c2860e684a
CesarRodriguez10 <alejorz2004@gmail.com>
2026-03-16 15:57:15 -0500

    Importaciones en el Main

--- app/src/main/java/com/example/cjcbowling/MainActivity.kt
+++ app/src/main/java/com/example/cjcbowling/MainActivity.kt
@@ -1,47 +1,9 @@
-package com.example.cjcbowling
+package com.example.bowlingapp.view
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.cjcbowling.ui.theme.CJCBowlingTheme
-//
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            CJCBowlingTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CJCBowlingTheme {
-        Greeting("Android")
-    }
-}
+import com.example.bowlingapp.data.ReservationPreferences
+import com.example.bowlingapp.repository.ReservationRepository
+import com.example.bowlingapp.view.screens.DashboardScreen
+import com.example.bowlingapp.viewmodel.ReservationViewModel
